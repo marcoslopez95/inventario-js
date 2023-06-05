8090fd6ae4ec7e9a17dee3f996c9d6e88597f09c
Marcos L <marcoslopez1895@gmail.com>
2023-06-05 01:35:02 -0400

    add categories crud complete

--- src/product/relations/categories/categories.service.ts
+++ src/product/relations/categories/categories.service.ts
@@ -1,26 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Model } from 'src/helpers/model.interface';
+import { Category } from './entities/category.entity';
 
 @Injectable()
-export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
-  }
-
-  findAll() {
-    return `This action returns all categories`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
-  }
-
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} category`;
-  }
-}
+export class CategoriesService extends Model<
+  Category,
+  CreateCategoryDto,
+  UpdateCategoryDto
+> {}
