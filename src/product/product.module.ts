5c958c5a1bacfa49c6c3e78eeef3f9f457685387
Marcos L <marcoslopez1895@gmail.com>
2023-06-05 01:30:59 -0400

    init categories

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { BrandModule } from './relations/brand/brand.module';
+import { CategoriesModule } from './relations/categories/categories.module';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService],
-  imports: [BrandModule],
+  imports: [BrandModule, CategoriesModule],
 })
 export class ProductModule {}
